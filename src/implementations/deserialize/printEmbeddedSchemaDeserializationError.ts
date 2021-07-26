eee4bcddc35be60278264d61074e8f94afadddb8
corno <gitkraken.a@schraverus.com>
2021-07-26 14:22:35 +0200

    .

--- src/implementations/deserialize/printEmbeddedSchemaDeserializationError.ts
+++ src/implementations/deserialize/printEmbeddedSchemaDeserializationError.ts
@@ -5,12 +5,6 @@ function assertUnreachable<RT>(_x: never): RT {
     throw new Error("unreachable")
 }
 
-/**
- * a Document needs a schema to be validated. Besides the basic schema structure we want to be able to report additional
- * validation errors.
- * All built-in schemas create a combination of these 2
- */
-
 export function printEmbeddedSchemaDeserializationError(error: SchemaDeserializationError): string {
     switch (error[0]) {
         case "expect": {
